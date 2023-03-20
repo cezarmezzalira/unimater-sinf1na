9cf5c4af952d81f56c1463105cc45117d6f30f2d
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2023-03-20 00:25:56 -0300

    feat: Aula 10 - Estruturas de repetição com while

---
+++ aula10-exemplos/src/Exemplo04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exemplo04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char resposta = 'S';
+
+        while (resposta != 'N') {
+            System.out.println("Informe um valor: ");
+            int valor_n = scanner.nextInt();
+
+            int valor_r = valor_n * 3;
+
+            System.out.println(valor_r);
+
+            System.out.println("Deseja continuar?");
+
+            resposta = scanner.next().charAt(0);
+
+        }
+
+        scanner.close();
+    }
+}
